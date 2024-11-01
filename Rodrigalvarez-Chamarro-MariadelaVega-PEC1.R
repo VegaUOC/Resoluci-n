c5b3b627c1425b7e3b49b76ab152b3b6b7738db4
Vega Rodrigálvarez <vrodrigalvarez@gmail.com>
2024-11-02 00:12:32 +0100

    Generación del contenedor se SummarizedExperiment

--- Rodrigalvarez-Chamarro-MariadelaVega-PEC1.R
+++ Rodrigalvarez-Chamarro-MariadelaVega-PEC1.R
@@ -1,6 +1,12 @@
+##### Carga de librerias
+
 # Carga de librerías necesarias para la realización de la PEC
 
 # Librería Bioconductor
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("SummarizedExperiment")
+library("SummarizedExperiment")
 
 # Paquete de trabajo para la carga de datos en excelz
 if (!require(readxl)){
@@ -8,10 +14,20 @@ if (!require(readxl)){
   library(readxl)
 }
 
+##### Directorio de trabajo
 
 # Establece el directorio de trabajo
-setwd(paste0("C:/ITA - Division/Formacion/2023/Máster en Biocomputación y BioInformática/Semestre 04/",
-             "07 Análisis Datos Ómicos/PEC1/Resolucion/"))
+getwd()
 
+# Leer los datos desde el archivo Excel
+samples_data <- read_xlsx("./data/GastricCancer/GastricCancer_NMR.xlsx",sheet = "Data",col_names = TRUE)
+row_data <- read_xlsx("./data/GastricCancer/GastricCancer_NMR.xlsx",sheet = "Peak",col_names = TRUE)
 
-# Cargar la librería
+# Crear los conjuntos de datos para generar el Summarized Experiment
+samples_matrix <- t(as.matrix(samples_data[,5:153]))
+colnames(samples_matrix) <- unlist(samples_data[,2])
+rows_info <- DataFrame(row_data[,2:5])
+cols_info <- DataFrame(samples_data[,2:4])
+
+se <- SummarizedExperiment(assays=samples_matrix, colData=cols_info, rowData = rows_info)
+se
